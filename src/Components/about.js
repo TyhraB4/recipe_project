2eec95d2bb0fbadf1fab998b90d71872001aa1b4
Ashley Beal <ashleybeal86@yahoo.com>
2022-09-11 09:46:30 -0400

    updated READMD.md file

--- src/Components/about.js
+++ src/Components/about.js
@@ -5,7 +5,7 @@ function About() {
       <div>
         <p>
           Team ATTCK Recipes is a bad-ass recipe website created by Ashley Beal,
-          Taylor Forbes, Thyra Barreto, Keandre Benjamin, and Carson Neagle
+          Taylor Forbes, Thyra Barreto, Keandre Benjamin, and Carson Neagle.gigit add 
         </p>
       </div>
     </main>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const router = require("express").Router();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
