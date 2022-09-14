63a997ae5a757a1b0d3f8e7a3b7c7502b88ff5c1
Ashley Beal <ashleybeal86@yahoo.com>
2022-09-13 21:04:18 -0400

    added react router dom and changed app.js

--- src/App.js
+++ src/App.js
@@ -1,99 +1,48 @@
 import "./App.css";
-import { Navbar, Container, Nav, Form, Button, NavDropdown, Carousel, Card } from "react-bootstrap";
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import Home from "./src/components/home.js";
+import About from ".src/Components/about.js";
+import Packages from "./components/Packages";
 
 function App() {
+  const recipes = [
+    "Spicy Chicken Pad Thai",
+    "Strawberry Jalapeno Baby Back Ribs",
+    "Chocolate Chip Espresso Icebox Cake",
+  ];
+
   return (
-    <main>
-      <div>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand href="#home">ATTCK Recipe's ™</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">About Us</Nav.Link>
-                <NavDropdown title="Recipes" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">
-                    Find Recipes
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Add a New Recipe
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Update or Delete a Recipe
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+    <div className="App">
+      <Router>
+        <header>
+          <h1 className="title">ATTCK Recipe's ™</h1>
 
-        <h1>ATTCK Recipe's ™</h1>
-      </div>
-      <div>
-        <img
-          src="https://images.unsplash.com/reserve/EnF7DhHROS8OMEp2pCkx_Dufer%20food%20overhead%20hig%20res.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1178&q=80"
-          alt="Home Page Single Pic"
-        />
-        <hr></hr>
-        <br></br>
-      </div>
-      <h2 className="trending">New Recipes Trending Now</h2>
-      <br></br>
-      <Carousel>
-        <Carousel.Item>
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://www.kitchenstories.com/_next/image?url=https%3A%2F%2Fimages.services.kitchenstories.io%2FT9G8LrleROVGrSCX9pzqq48sMNM%3D%2F768x576%2Fimages.kitchenstories.io%2FrecipeImages%2FRP16_01_42_FINAL.jpg&w=1440&q=85"
-              alt=""
-            />
-            <Card.Body>
-              <Card.Title>Chocolate Chip Espresso Icebox Cake</Card.Title>
-              <Card.Text>Yummy chocolate cake...no oven required</Card.Text>
-              <Button variant="primary">Check out Recipe Here</Button>
-            </Card.Body>
-          </Card>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src=".../public/strawberry-ribs.jpg"
-            alt="Chorizo and Kale Taco"
-          />
+          <div className="Navbar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/recipes">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
 
-          <Carousel.Caption>
-            <h3>Kale Chorizo Tacos</h3>
-            <p>
-              Quick and easy spicy chorizo tacos with kale, mushrooms and
-              avacado crema.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src=""
-            alt="Strawberry Jalapeno Ribs"
+        <div className="display">
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About Us} />
+          <Route
+            path="/recipes"
+            render={() => <Recipes recipes={recipes} />}
           />
-
-          <Carousel.Caption>
-            <h3>Strawberry Jalapeno Baby Back Ribs</h3>
-            <p>Awesome ribs with a little heat and a little sweet.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </main>
+        </div>
+      </Router>
+    </div>
   );
 }
 
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,38 +1,75 @@
-import React from "react";
-import "./index.css";
-import Carousel from "react-bootstrap/Carousel";
 import "./App.css";
+import { Navbar, Container, Nav, Form, Button, NavDropdown, Carousel, Card,} from "react-bootstrap";
 
 function Home() {
   return (
     <main>
       <div>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">ATTCK Recipe's ™</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">About Us</Nav.Link>
+                <NavDropdown title="Recipes" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">
+                    Find Recipes
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Add a New Recipe
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Update or Delete a Recipe
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+
         <h1>ATTCK Recipe's ™</h1>
       </div>
-      <hr></hr>
-      <h3 className="trending">New Recipes Trending Now</h3>
+      <div>
+        <img
+          src="https://images.unsplash.com/reserve/EnF7DhHROS8OMEp2pCkx_Dufer%20food%20overhead%20hig%20res.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1178&q=80"
+          alt="Home Page Single Pic"
+        />
+        <hr></hr>
+        <br></br>
+      </div>
+      <h2 className="trending">New Recipes Trending Now</h2>
       <br></br>
       <Carousel>
         <Carousel.Item>
-          <img
-            height={900}
-            width={20}
-            className="d-block w-100"
-            src="https://www.kitchenstories.com/_next/image?url=https%3A%2F%2Fimages.services.kitchenstories.io%2FT9G8LrleROVGrSCX9pzqq48sMNM%3D%2F768x576%2Fimages.kitchenstories.io%2FrecipeImages%2FRP16_01_42_FINAL.jpg&w=1440&q=85"
-            alt="Yummy Chocolate Cake Pic"
-          />
-          <Carousel.Caption>
-            <h3>Chocolate Chip Espresso Icebox Cake</h3>
-            <p>Yummy chocolate cake...no oven required</p>
-          </Carousel.Caption>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://www.kitchenstories.com/_next/image?url=https%3A%2F%2Fimages.services.kitchenstories.io%2FT9G8LrleROVGrSCX9pzqq48sMNM%3D%2F768x576%2Fimages.kitchenstories.io%2FrecipeImages%2FRP16_01_42_FINAL.jpg&w=1440&q=85"
+              alt=""
+            />
+            <Card.Body>
+              <Card.Title>Chocolate Chip Espresso Icebox Cake</Card.Title>
+              <Card.Text>Yummy chocolate cake...no oven required</Card.Text>
+              <Button variant="primary">Check out Recipe Here</Button>
+            </Card.Body>
+          </Card>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            height={900}
-            width={20}
             className="d-block w-100"
-            src="https://www.mymoderncookery.com/wp-content/uploads/2016/05/DSC_0285-665x1024.jpg.webp"
-            alt="Chorizo and Kale Taco Pic"
+            src=".../public/strawberry-ribs.jpg"
+            alt="Chorizo and Kale Taco"
           />
 
           <Carousel.Caption>
@@ -45,11 +82,9 @@ function Home() {
         </Carousel.Item>
         <Carousel.Item>
           <img
-            height={950}
-            width={20}
             className="d-block w-100"
-            src="https://www.mymoderncookery.com/wp-content/uploads/2018/05/Curlys-Strawberry-Jalapeno-Ribs-6-of-8-1076x1624.jpg.webp"
-            alt="Strawberry Jalapeno Ribs Pic"
+            src=""
+            alt="Strawberry Jalapeno Ribs"
           />
 
           <Carousel.Caption>
